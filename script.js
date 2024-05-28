71a087e883f4d2162015b6c0e18a1cdf2c3f1c06
FiveDragonDev <FiveDragonDev@gmail.com>
2024-05-28 23:33:51 +0300

    patch 1

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
-var count = 0;
-
+var count;
 load();
-
-var button = document.getElementById("click");
+var clicker_button = document.getElementById("click");
+var reset_button = document.getElementById("reset");
 var counter = document.getElementById("counter");
-button.addEventListener("click", function () {
+clicker_button.addEventListener("click", click);
+reset_button.addEventListener("click", reset)
+
+function click(){
     count++;
     counter.innerHTML = count.toString();
     save();
-});
+}
 
 function save() {
     localStorage.setItem('count', count);
 }
-
 function load() {
+    var counter = document.getElementById("counter");
     var savedContent = localStorage.getItem('count');
     if (savedContent) {
-        var counter = document.getElementById("counter");
         count = savedContent;
         counter.innerHTML = savedContent;
     }
+    else {
+        count = 0;
+        counter.innerHTML = "click here!";
+    }
+}
+function reset() {
+    localStorage.clear();
+    count = 0
+    counter.innerHTML = "click here!";
 }
